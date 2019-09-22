5412e8493242f10fcf5aff64d4093d0de419896b
13circle <13circle@gmail.com>
2019-09-22 09:01:49 +0900

    MJUClassRoomRsvn Repo

---
+++ MJUClassRoomRsvn/app/src/main/java/com/example/mjuclassroomrsvn/CalendarActivity.java
@@ -0,0 +1,31 @@
+package com.example.mjuclassroomrsvn;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //
+            }
+        });
+
+    }
+
+}
---
+++ MJUClassRoomRsvn/app/src/main/java/com/example/mjuclassroomrsvn/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mjuclassroomrsvn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, CalendarActivity.class);
+        this.startActivity (intent);
+    }
+}
+
